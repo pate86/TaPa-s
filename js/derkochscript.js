b2401ee4217785a5c7450316e1ab6b8de6b43776
Talha S. <talha.sahin@docc.techstarter.de>
2024-01-10 15:14:22 +0100

    Api in app.js eingebunden und chatbox in das fenster besser positioniert

--- js/derkochscript.js
+++ js/derkochscript.js
@@ -5,7 +5,39 @@ const btnEl = document.querySelector(".fa-paper-plane");
 const cardBodyEl = document.querySelector(".card-body");
 
 let userMessage;
+const OPENAI_API_KEY = "API_KEY"; //  muss eingefüpgt werden !!!
+const URL = "https://api.openai.com/v1/chat/completions";
 
+const chatGenerator = () => {
+    const requestOption = {
+        method : "POST",
+        headers: {
+            "content-type": "application/json",
+            autorization : `Bearer ${OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+            modal: "gpt-3.5-turbo-16k",
+            message: [
+                {
+                role: "system",
+                content : userMessage,
+                }
+            ],
+        }),
+
+    };
+
+    fetch(URL, requestOption)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        });
+
+};
+
+
+
+// manage chat
 function manageChat() {
     userMessage = inputEl.value.trim();
 
@@ -16,12 +48,13 @@ function manageChat() {
 
     setTimeout(() => {
         cardBodyEl.appendChild(messageEl("Ich schicke dir gleich etwas Köstliches zu, Augenblick bitte. . .", "chat-bot"));
+        chatGenerator();
     }, 600);
 }
 
 
 //messages 
-const messageEl = (message,className) =>{
+const messageEl = (message, className) =>{
     const chatEl = document.createElement("div");
     chatEl.classList.add("chat", `${className}`);
     let chatContent = 
