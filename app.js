4c4e24892ca4ece5623f68629210ec77c84d67a5
Talha S. <talha.sahin@docc.techstarter.de>
2024-01-11 02:09:47 +0100

    Ein mann armee

--- app.js
+++ app.js
@@ -22,17 +22,20 @@ const openai = new OpenAI({
 const assistant = await openai.beta.assistants.retrieve(
 	"asst_AEUXuvM6wzra6jn0r62MUcSu"
 );
-console.log(assistant);
+//console.log(assistant);
 
 //Threads
 // Threads variable ertellen. (ID wird sichtbar im terminal) 
 //const thread = await openai.beta.threads.create();
 
 // Message variable erstellen um es an threads variable hinzuzufügen
-//const message = await openai.beta.threads.messages.create(thread.id, {
-//	role: "user",
-//	content: "was macht 50 * 50"
-//});
+const message = await openai.beta.threads.messages.create(
+	thread.id,
+	{
+	  role: "user",
+	  content: "I need to solve the equation `3x + 11 = 14`. Can you help me?"
+	}
+  );
 
 // RUN ASSISTANT 
 //const run = await openai.beta.threads.runs.create(thread.id, {	
@@ -41,22 +44,22 @@ console.log(assistant);
 //});
 
 // RUN ASSISTANT um den neuen status anzuzeigen von zeile 37-41 (zeile 37-41 ausklammern)
-//const run = await openai.beta.threads.runs.retrieve(
-//	"thread_djYKHVoVDgIKCuNvv3ycGPq4",
-//	"run_Cax9AvMJDTIhQCCxlWqO1RLA"
-//);
+const run = await openai.beta.threads.runs.retrieve(
+	"thread_djYKHVoVDgIKCuNvv3ycGPq4",
+	"run_Cax9AvMJDTIhQCCxlWqO1RLA"
+);
 
 //console.log(run);
 
-//const messages = await openai.beta.threads.messages.list(
-//	"thread_djYKHVoVDgIKCuNvv3ycGPq4"
-//);
+const messages = await openai.beta.threads.messages.list(
+	"thread_djYKHVoVDgIKCuNvv3ycGPq4"
+);
 
-//console.log(messages);
+console.log(messages);
 
-//messages.body.data.forEach(message => {
-//	console.log(message.content);
-//});
+messages.body.data.forEach(message => {
+	console.log(message.content);
+});
 
 const logs = await openai.beta.threads.runs.steps.list(
 	"thread_djYKHVoVDgIKCuNvv3ycGPq4",
@@ -71,3 +74,4 @@ logs.body.data.forEach((log) => {
 	console.log(log.step_details);
 });
 console.log(logs);
+
